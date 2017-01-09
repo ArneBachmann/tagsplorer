78d810c68411be8fd7c24cd3286e9107a88e41db
ArneBachmann <ArneBachmann@users.noreply.github.com>
2017-01-09 19:22:03 +0100

    Fixes #12

--- setup.py
+++ setup.py
@@ -1,9 +1,12 @@
-import sys
 import os
+import subprocess
+import sys
 import time
-from setuptools import setup
 
-# Upload to PyPI by running setup.py clean build_py sdist bdist upload with a correctly set up ~/.pypirc (need HOME variable set correctly on Windows)
-
+micro = ""
+if os.path.exists(".git"):
+  p = subprocess.Popen("git describe --always", shell = True, bufsize = 1, stdout = subprocess.PIPE)
+  so, se = p.communicate()
+  micro = so.strip()
 with open("version.py", "w") as fd:  # create version string at build time
-  fd.write(time.strftime("__version_info__ = tuple(int(_) for _ in ('%Y', '%m%d', '%H%M'))\n__version__ = '.'.join(map(str, __version_info__))\n"))
+  fd.write(time.strftime("__version_info__ = tuple(int(_) for _ in ('%Y', '%m%d', '%H%M'))\n__version__ = '.'.join(map(str, __version_info__)) + '-git-%%s'\n") % micro)
