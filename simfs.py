0270c603c91eabcfdcaf3a3a40e49aae0dc70a6d
ArneBachmann <ArneBachmann@users.noreply.github.com>
2017-07-25 08:34:53 +0200

    Fixed

--- simfs.py
+++ simfs.py
@@ -10,8 +10,10 @@ _log.debug("Using virtual case-insensitive file system")
 if sys.version_info.major >= 3:
   import io
   file = io.IOBase
+  _types = (str, bytes)
 else:
   import dircache  # in addition to os.listdir
+  _types = (str, bytes, unicode)
 
 
 # Patch existence function
@@ -48,7 +50,7 @@ def exists(path, get = False):
   '''
   _log.debug("Patched os.path.exists %r" % path)
   if type(path) is file: return _exists(path)  # file handle
-  if not isinstance(path, (str, bytes)):
+  if not isinstance(path, _types):
     if not get: return _exists(path)  # delegate or force original Exception
     raise ValueError("Unknown argument type %r" % type(path))
   if path is None or path == "": return _exists(path) if not get else path
