9e96d65fdc782c7f3d54769fbca58b22d5a1d139
Arne Bachmann <ArneBachmann@users.noreply.github.com>
2021-02-06 16:04:36 +0100

    Added missing files

---
+++ tagsplorer/__main__.py
@@ -0,0 +1,8 @@
+# coding=utf-8
+
+''' tagsPlorer package entry point  (C) 2021-2021  Arne Bachmann  https://github.com/ArneBachmann/tagsplorer '''
+
+from tagsplorer import tp
+
+
+tp.Main().parse_and_run()
